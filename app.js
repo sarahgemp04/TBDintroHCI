8a6e45a676e37d3211b4d7627faeda12278d7c58
Thomas Loh <tloh@ucsd.edu>
2017-02-02 09:46:29 -0800

    told to commit

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+
+var index = require('./routes/index');
+var project = require('./routes/project');
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
@@ -36,6 +41,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
