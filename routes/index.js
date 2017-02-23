3777d676b1b0b679a6f1bf42acd138db6152cc5e
Thomas Loh <tloh@ucsd.edu>
2017-02-22 20:12:41 -0800

    working on database solution

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@
  */
 
 exports.view = function(req, res){
+
+var jsonfile = require('jsonfile')
+	var file = '../tmp/data.json';
+jsonfile.readFile(file, function(err, obj) {
+  console.dir(obj)
+})
+	
+	//jsonfile.writeFile(file, obj, function(err) {console.error(err)});
   res.render('index', {
     //nothing to do
   });
