6b6ee7371dd051a16f2e0f5fe7c959ea660596c7
Sarah Gemperle <sgemperl@ucsd.edu>
2017-02-24 13:53:12 -0800

    Have one slight bug. Only shows functionality for calendar Friday's :(

--- routes/bodyMap.js
+++ routes/bodyMap.js
@@ -5,17 +5,19 @@ exports.viewBodyMap = function(req, res) { 
   if(req.query.id == undefined) {
     var bodyMap = "Body Map"
   	res.render('bodyMap', {
-  	 'title': bodyMap
+  	 'title': bodyMap,
+     'day': ""
   });
   } else {
   	var day = req.query.id;
     console.log(day);
-    day = JSON.parse(JSON.stringify(days.days.day));
-    console.log(day);
-    weekDay = JSON.parse(JSON.stringify(day["title"]));
-  	console.log(weekDay);
-  	res.render('bodyMap?id=' + day, {
-  		'title': weekDay
+    //day = JSON.parse(JSON.stringify(days.days.day));
+    //console.log(day);
+    //weekDay = JSON.parse(JSON.stringify(day["title"]));
+  	//console.log(weekDay);
+  	res.render('bodyMap', {
+  		'title': "Friday",
+      'day': req.query.id
   	});
   }
 
--- routes/dailylog.js
+++ routes/dailylog.js
@@ -1,3 +1,5 @@
+var days = require('../days.json');
+
 exports.viewDailyLog = function(req, res) { 
   res.render('dailylog', {
 
