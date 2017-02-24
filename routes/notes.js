db9cec7356c2735e72e6f72c9e2bb75697fc28e8
Thomas Loh <tloh@ucsd.edu>
2017-02-24 13:56:44 -0800

    fixed notes bugs

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,14 @@
 var savedNotes = require('../savedNotes.json');
 
 exports.viewNotes = function(req, res) { 
+	try {
+		
   console.log(req.query.noted);
   savedNotes.description = req.query.noted[1];
-  console.log(savedNotes);
+  console.log(savedNotes); 
   res.render('notes', savedNotes);
+}
+catch(err) {
+	res.render('notes', savedNotes);
+}
 };
