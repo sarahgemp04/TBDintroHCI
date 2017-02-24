82c6e70f03ea89661b35b9106d403074e5de4ec8
Sarah Gemperle <sgemperl@ucsd.edu>
2017-02-24 14:00:49 -0800

    Can now view which day you are adding to at bottom of View

--- public/js/dailylog.js
+++ public/js/dailylog.js
@@ -1,6 +1,7 @@
 'use strict';
 var isHidden = true;
 var href = "";
+var pText = "";
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -17,10 +18,13 @@ function initializePage() {
 	if(!isHidden) {
 		$("#calendarAddDiv").css("display", "inline-block");
 		$("#calendarAddDiv a").attr("href", href);
+		$("#calendarAddDiv p").text(window.pText);
+
 		console.log(href);
 		console.log($("#calendarAddDiv a").attr("href"));
+
 	} else {
-		$("#calendarAddDiv").css("display", "none")
+		$("#calendarAddDiv").css("display", "none");
 	}
 
 	//$(".days.day").click(dayClicked($(this).attr("id")));
@@ -31,7 +35,8 @@ function dayClicked(day) {
 	console.log("Function was called" + day );
 	$("#calendarAddDiv").css("display", "inline-block");
 	window.href = "bodyMap?id=" + day;
-
+	window.pText = "Add a workout to the " + day;
+	console.log(pText);
 	window.isHidden = false;
 	initializePage();
 	return false;
