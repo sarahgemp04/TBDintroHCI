9f3955f64606aa207ac11ef567451de6f621b735
Thomas Loh <tloh@ucsd.edu>
2017-03-08 18:55:38 -0800

    interesting changes

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,14 +1,22 @@
 
+// var allNotes = require('../../savedNotes.json');
+// var userJSON = require('../../user.json');
 //Notes js
 $( document ).ready(function() {
     console.log( "ready!" );
 });
 
+/*
 $('#saveNote').submit(function(e) {
 	e.preventDefault();
 
 	var notes = $('#areaNote').val();
-
+	var currDate = allNotes["date"];
     // Send the POST request
-    $.post('addTask', { taskName: taskName }, postCallback);
+    $.get('/notes', allNotes , saveFun);
 });
+
+function saveFun(result) {
+
+}
+*/
--- routes/notes.js
+++ routes/notes.js
@@ -9,13 +9,18 @@ exports.viewNotes = function(req, res) { 
 		console.log("entered new date in notes: " + userJSON.date);
 	}
 	try {
-		console.log(savedNotes[currDate]);
 		var currDate = userJSON["date"];
-  savedNotes[currDate] = req.query.noted[1];
-  console.log(savedNotes);
-  res.render('notes', savedNotes);
-}
-catch(err) {
-	res.render('notes', savedNotes);
-}
+		savedNotes[currDate] = req.query.noted[0];
+		console.log("save data is:" + req.query.noted[0]);
+		savedNotes['date'] = currDate;
+		console.log("finished try");
+		console.log("savedNotes is: " + JSON.stringify(savedNotes));
+		res.render('notes', savedNotes);
+	}
+	catch(err) {
+		console.log("entered catch");
+		savedNotes['date'] = userJSON["date"];
+		console.log("savedNotes is: " + JSON.stringify(savedNotes));
+		res.render('notes', savedNotes);
+	}
 };
